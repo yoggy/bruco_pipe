90828310adbe9146b81568f4037e7bf4423e3d9b
yoggy <yoggy0@gmail.com>
2013-08-04 19:58:04 +0900

    modified:   log.cpp

--- log.cpp
+++ log.cpp
@@ -5,6 +5,14 @@
 
 #include <time.h>
 
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#include <string.h>
 #include <string>
 #include "string.hpp"
 
